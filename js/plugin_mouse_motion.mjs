75a580cbaa055861c20dd05b47af5ef858ec9753
coolguy284 <coolguy284v64@gmail.com>
2026-07-22 21:13:13 -0400

    mostly finish mousemover

--- js/plugin_mouse_motion.mjs
+++ js/plugin_mouse_motion.mjs
@@ -21,9 +21,15 @@ export class MouseMover {
   #mouseDown = false;
   #screenVelX = 0;
   #screenVelY = 0;
+  #screenVelMag = 0;
   #timeUnclicked = null;
+  #pMouseX = null;
+  #pMouseY = null;
+  #previousMouseDrags = [];
   
+  #mouseMotionElem = null;
   #moveViewCallback = null;
+  #scaleViewCallback = null;
   
   async #movementLoop() {
     if (this.#movementLoopRunning) return;
@@ -38,7 +44,7 @@ export class MouseMover {
           Math.abs(this.#screenVelX) >= MouseMover.#INERTIA_MOVE_THRESHOLD ||
           Math.abs(this.#screenVelY) >= MouseMover.#INERTIA_MOVE_THRESHOLD,
         processZoom =
-          Math.abs(Math.log(targetScale / getScale())) >= MouseMover.#INERTIA_ZOOM_THRESHOLD;
+          Math.abs(Math.log(this.#targetScaleDelta)) >= MouseMover.#INERTIA_ZOOM_THRESHOLD;
       
       let lastFrameTime;
       
@@ -50,27 +56,27 @@ export class MouseMover {
       
       if (processMovement) {
         if (!this.#mouseDown) {
-          this.#moveViewCallback(this.#screenVelX * lastFrameTime, this.#screenVelY * lastFrameTime);
+          this.#moveViewCallback(this.#screenVelX, this.#screenVelY);
           
           if (pTimestamp && this.#timeUnclicked != null) {
             const timeSinceUnclicked = (timestamp - this.#timeUnclicked) / 1000;
             
             let newVelMag;
             
-            if (timeSinceUnclicked > MouseMover.#INERTIA_FASTSLOWDOWN_TIME_THRESHOLD && screenVelMag > MouseMover.#INERTIA_FASTSLOWDOWN_VEL_THRESHOLD) {
+            if (timeSinceUnclicked > MouseMover.#INERTIA_FASTSLOWDOWN_TIME_THRESHOLD && this.#screenVelMag > MouseMover.#INERTIA_FASTSLOWDOWN_VEL_THRESHOLD) {
               // bigger for fast speeds
-              newVelMag = Math.max((screenVelMag - MouseMover.#INERTIA_SLOWDOWN * lastFrameTime) * MouseMover.#INERTIA_SLOWDOWN_FACTOR ** lastFrameTime, 0);
+              newVelMag = Math.max((this.#screenVelMag - MouseMover.#INERTIA_SLOWDOWN * lastFrameTime) * MouseMover.#INERTIA_SLOWDOWN_FACTOR ** lastFrameTime, 0);
             } else {
               // linear for slow speeds
-              newVelMag = Math.max(screenVelMag - MouseMover.#INERTIA_SLOWDOWN * lastFrameTime, 0);
+              newVelMag = Math.max(this.#screenVelMag - MouseMover.#INERTIA_SLOWDOWN * lastFrameTime, 0);
             }
             
-            let slowdownFactor = screenVelMag != 0 ? newVelMag / screenVelMag : 0;
+            const slowdownFactor = this.#screenVelMag != 0 ? newVelMag / this.#screenVelMag : 0;
             
             this.#screenVelX *= slowdownFactor;
             this.#screenVelY *= slowdownFactor;
             
-            screenVelMag = newVelMag;
+            this.#screenVelMag = newVelMag;
           }
         }
       }
@@ -78,9 +84,9 @@ export class MouseMover {
       // zoom processing
       
       if (processZoom && pTimestamp) {
-        let scaleFactor = Math.exp(Math.log(targetScale / getScale()) * Math.min(MouseMover.#INERTIA_ZOOM_FACTOR * lastFrameTime, 1));
+        const scaleFactor = Math.exp(Math.log(this.#targetScaleDelta) * Math.min(MouseMover.#INERTIA_ZOOM_FACTOR * lastFrameTime, 1));
         
-        setScale(getScale() * scaleFactor);
+        this.#scaleViewCallback(scaleFactor);
       }
       
       // call next iteration of loop
@@ -95,7 +101,7 @@ export class MouseMover {
       if (
         Math.abs(this.#screenVelX) < MouseMover.#INERTIA_MOVE_THRESHOLD &&
         Math.abs(this.#screenVelY) < MouseMover.#INERTIA_MOVE_THRESHOLD &&
-        Math.abs(Math.log(targetScale / getScale())) < MouseMover.#INERTIA_ZOOM_THRESHOLD
+        Math.abs(Math.log(this.#targetScaleDelta)) < MouseMover.#INERTIA_ZOOM_THRESHOLD
       ) {
         this.#movementLoopRunning = false;
         pTimestamp = null;
@@ -118,81 +124,92 @@ export class MouseMover {
   
   constructor({
     mouseMotionElem,
-    // moveViewCallback(xAmount, yAmount): called when view should be moved by the scale-invariant amount; scaled based on frame time
+    // moveViewCallback(xAmount, yAmount): called when view should be moved by the scale-invariant amount
     moveViewCallback,
+    // scaleViewCallback(scaleMultiplier): called when view scale should be multiplied by the given amount
+    scaleViewCallback,
   }) {
+    this.#mouseMotionElem = mouseMotionElem;
+    
     mouseMotionElem.addEventListener('mousedown', e => {
-      const x = e.x, y = e.y;
+      const x = e.offsetX, y = e.offsetY;
       
       this.#mouseDown = true;
       
-      pMouseX = x;
-      pMouseY = y;
+      this.#pMouseX = x;
+      this.#pMouseY = y;
     });
     
     mouseMotionElem.addEventListener('mouseup', e => {
       this.#timeUnclicked = performance.now();
       
-      let minValidTime = this.#timeUnclicked - MouseMover.#PREV_MOUSE_BUFFER_TIMESPAN;
+      const minValidTime = this.#timeUnclicked - MouseMover.#PREV_MOUSE_BUFFER_TIMESPAN;
       
-      let mouseDragSum = previousMouseDrags.filter(x => x[2] > minValidTime).reduce((a, c) => [a[0] + c[0], a[1] + c[1]], [0, 0]);
-      let mouseDrag = previousMouseDrags.length ? [mouseDragSum[0] / previousMouseDrags.length, mouseDragSum[1] / previousMouseDrags.length] : [0, 0];
+      const mouseDragSum =
+        this.#previousMouseDrags
+          .filter(x => x[2] > minValidTime)
+          .reduce((a, c) => [a[0] + c[0], a[1] + c[1]], [0, 0]);
+      
+      const mouseDrag =
+        this.#previousMouseDrags.length ?
+          [
+            mouseDragSum[0] / this.#previousMouseDrags.length,
+            mouseDragSum[1] / this.#previousMouseDrags.length,
+          ] :
+          [0, 0];
       
       this.#screenVelX = mouseDrag[0];
       this.#screenVelY = mouseDrag[1];
       
-      previousMouseDrags.splice(0, Infinity);
+      this.#previousMouseDrags.splice(0, Infinity);
       
       this.#mouseDown = false;
     });
     
     mouseMotionElem.addEventListener('mousemove', e => {
-      let x = e.x, y = e.y;
+      const x = e.offsetX, y = e.offsetY;
       
       if (this.#mouseDown) {
-        this.#screenVelX = x - pMouseX;
-        this.#screenVelY = -y + pMouseY;
+        this.#screenVelX = x - this.#pMouseX;
+        this.#screenVelY = -y + this.#pMouseY;
         
-        screenVelMag = Math.hypot(this.#screenVelX, this.#screenVelY);
+        this.#screenVelMag = Math.hypot(this.#screenVelX, this.#screenVelY);
         
-        shiftShipPos(this.#screenVelX, this.#screenVelY);
+        this.#moveViewCallback(this.#screenVelX, this.#screenVelY);
       }
       
-      pMouseX = x;
-      pMouseY = y;
+      this.#pMouseX = x;
+      this.#pMouseY = y;
       
-      previousMouseDrags.push([this.#screenVelX, this.#screenVelY, performance.now()]);
-      if (previousMouseDrags.length > MouseMover.#PREV_MOUSE_BUFFER_LENGTH) {
-        previousMouseDrags.splice(0, 1);
+      this.#previousMouseDrags.push([this.#screenVelX, this.#screenVelY, performance.now()]);
+      if (this.#previousMouseDrags.length > MouseMover.#PREV_MOUSE_BUFFER_LENGTH) {
+        this.#previousMouseDrags.splice(0, 1);
       }
       
       if (this.#mouseDown) {
-        movementLoop();
+        this.#movementLoop();
       }
     });
     
     mouseMotionElem.addEventListener('wheel', e => {
-      let wheelDelta = e.wheelDelta;
+      const wheelDelta = e.wheelDelta;
       
-      pMouseX = getRealCanvasWidth() / 2;
-      pMouseY = getRealCanvasHeight() / 2;
+      [this.#pMouseX, this.#pMouseY] = MouseMover.#getElementDimensions(this.#mouseMotionElem);
       
-      let scaleFactor = MouseMover.#ZOOM_SCALE_FACTOR ** wheelDelta;
+      const scaleFactor = MouseMover.#ZOOM_SCALE_FACTOR ** wheelDelta;
       
-      targetScale *= scaleFactor;
-      
-      targetScalePMouseX = pMouseX;
-      targetScalePMouseY = pMouseY;
+      this.#targetScaleDelta *= scaleFactor;
       
       if (!this.#mouseDown) {
         this.#screenVelX = 0;
         this.#screenVelY = 0;
       }
       
-      movementLoop();
+      this.#movementLoop();
     });
     
     this.#moveViewCallback = moveViewCallback;
+    this.#scaleViewCallback = scaleViewCallback;
   }
   
   setTargetScaleDelta(targetScaleDelta) {
@@ -212,21 +229,14 @@ export class MouseMover {
   }
 }
 
-//let shiftShipPos;
 //let getRealCanvasWidth;
 //let getRealCanvasHeight;
-//let getScale;
-//let setScale;
 //
-//let screenVelMag;
-//let targetScalePMouseX, targetScalePMouseY;
-//let pMouseX, pMouseY;
 //let previousMouseDrags = [];
 
 //export function init(newGetScale, newSetScale, newShiftShipPos, newGetRealCanvasWidth, newGetRealCanvasHeight) {
 //  getScale = newGetScale;
 //  setScale = newSetScale;
-//  shiftShipPos = newShiftShipPos;
 //  getRealCanvasWidth = newGetRealCanvasWidth;
 //  getRealCanvasHeight = newGetRealCanvasHeight;
 //
